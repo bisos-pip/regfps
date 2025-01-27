73c49f49009e70ec903e7c4ffe3f7433b4a0ab14
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-01-26 18:06:41 -0800

    0.22 Modernized+.

--- py3/setup.py
+++ py3/setup.py
@@ -43,7 +43,7 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.21 -- forSysVersion=0.2 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.22 -- forSysVersion=0.2 -- constant=NA
 def pkgVersion():
         return '0.2'
 
@@ -52,7 +52,7 @@ def pkgVersion():
 # b:py3:pypi:setup/requires :extras ; :requirements "requirements.txt" (bring here requirements.txt)
 ####+BEGIN: b:py3:pypi:setup/requires :extras ()
 
-requires = [
+requires = [ 
 "blee",
 "blee.csPlayer",
 "bisos",
@@ -65,7 +65,7 @@ requires = [
 # b:py3:pypi:setup/scripts :comment
 ####+BEGIN: b:py3:pypi:setup/scripts :comment ""
 
-scripts = [
+scripts = [ 
 'bin/regFpsExample.cs',
 ]
 ####+END:
@@ -73,7 +73,7 @@ scripts = [
 # b:py3:pypi:setup/dataFiles :comment
 ####+BEGIN: b:py3:pypi:setup/dataFiles :comment "Instead of ./MANIFEST.in or in pyproject.toml"
 
-data_files = [
+data_files = [ 
 (' ',  ['lh-agpl3-LICENSE.txt', '_description.org', 'README.rst']),
 ]
 ####+END:
@@ -82,7 +82,7 @@ data_files = [
 ####+BEGIN: b:py3:pypi:setup/funcArgs :comment "defaults to --auto--"
 
 setuptools.setup(
-    name=pkgName(),  # bisos.regfps
+    name=pkgName(),  # 'bisos.regfps'
     version=pkgVersion(),
     packages=setuptools.find_packages(),
     scripts=scripts,
@@ -93,11 +93,9 @@ setuptools.setup(
     author_email='libre@mohsen.1.banan.byname.net',
     maintainer='Mohsen Banan',
     maintainer_email='libre@mohsen.1.banan.byname.net',
-    url='http://www.by-star.net/PLPC/180047',
     license='AGPL',
     description=description(),
     long_description=longDescription(),
-    download_url='http://www.by-star.net/PLPC/180047',
     install_requires=requires,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
