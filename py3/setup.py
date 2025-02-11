6b44fff27832be7bced4c2224644857aba9e6bf7
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-02-10 18:55:14 -0800

    0.23 csPlayer.

--- py3/setup.py
+++ py3/setup.py
@@ -43,7 +43,7 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.22 -- forSysVersion=0.2 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.23 -- forSysVersion=0.2 -- constant=NA
 def pkgVersion():
         return '0.2'
 
@@ -52,12 +52,13 @@ def pkgVersion():
 # b:py3:pypi:setup/requires :extras ; :requirements "requirements.txt" (bring here requirements.txt)
 ####+BEGIN: b:py3:pypi:setup/requires :extras ()
 
-requires = [ 
+requires = [
 "blee",
-"blee.csPlayer",
+"bisos.csPlayer",
 "bisos",
 "bisos.b",
 "bisos.common",
+"bisos.csPlayer",
 "bisos.usgAcct",
 ]
 ####+END:
@@ -65,7 +66,7 @@ requires = [
 # b:py3:pypi:setup/scripts :comment
 ####+BEGIN: b:py3:pypi:setup/scripts :comment ""
 
-scripts = [ 
+scripts = [
 'bin/regFpsExample.cs',
 ]
 ####+END:
@@ -73,7 +74,7 @@ scripts = [
 # b:py3:pypi:setup/dataFiles :comment
 ####+BEGIN: b:py3:pypi:setup/dataFiles :comment "Instead of ./MANIFEST.in or in pyproject.toml"
 
-data_files = [ 
+data_files = [
 (' ',  ['lh-agpl3-LICENSE.txt', '_description.org', 'README.rst']),
 ]
 ####+END:
